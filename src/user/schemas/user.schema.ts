1f8c6d139c10f0cbfde55d486cad66efec9bf617
MinhNguyenNhut <119271740+MinhNguyenNhut@users.noreply.github.com>
2026-09-16 12:00:19 +0700

    Update user.schema.ts

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -11,11 +11,17 @@ export type UserDocument = User & Document;
 
 @Schema({ collection: 'users', timestamps: true })
 export class User {
+
   @Prop({ trim: true })
   name?: string;
 
   @ApiProperty()
-  @Prop({ required: true, unique: true, lowercase: true, trim: true })
+  @Prop({
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  })
   email!: string;
 
   @ApiProperty()
@@ -23,24 +29,29 @@ export class User {
   passwordHash!: string;
 
   @ApiProperty({ enum: UserRole })
-  @Prop({ required: true, enum: Object.values(UserRole), default: UserRole.OWNER })
+  @Prop({
+    required: true,
+    enum: Object.values(UserRole),
+    default: UserRole.OWNER,
+  })
   role!: UserRole;
 
   createdAt!: Date;
+
   updatedAt!: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
-
-UserSchema.index({ email: 1 }, { unique: true });
 
 UserSchema.set('toJSON', {
   virtuals: true,
   versionKey: false,
   transform: function (_doc: any, ret: any) {
     ret.id = ret._id?.toString();
+
     delete ret._id;
     delete ret.passwordHash;
+
     return ret;
   },
 });
